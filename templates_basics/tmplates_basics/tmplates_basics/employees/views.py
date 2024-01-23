dfa4c3812e586a82a3dafb1f6ad18fcf087024ad
kdkaanev <kdkaanev@github.com>
2024-01-23 07:50:24 +0200

    new today

--- templates_basics/tmplates_basics/tmplates_basics/employees/views.py
+++ templates_basics/tmplates_basics/tmplates_basics/employees/views.py
@@ -1,11 +1,19 @@
+import datetime
+
 from django.shortcuts import render
 
 # Create your views here.
 
 class Person:
-    def __init__(self,first_name,last_name):
+    def __init__(self,first_name,last_name, age=None):
         self.first_name = first_name
         self.last_name = last_name
+        self.age = age
+
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 def index(request):
     context = {
         'title': 'Employees',
@@ -14,6 +22,10 @@ def index(request):
             "last_name": "Hujo"
         },
         "person_object":Person("John","Hujo"),
+        "ll":[1,2,3,4,5,6,7,8,9],
+        "date":datetime.date.today(),
+        "ages":[1,2,3,4,5,6,7,8,9],
+
     }
 
     return render(request,"employees/index.html", context)
