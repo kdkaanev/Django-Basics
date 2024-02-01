14643eed5a62aef2617e78bf600c96b31ea610a8
kdkaanev <kdkaanev@github.com>
2024-02-01 07:47:38 +0200

    tamplate advancet 0.2

--- tamplate_adv/tamp_adv/tamp_adv/web/views.py
+++ tamplate_adv/tamp_adv/tamp_adv/web/views.py
@@ -1,5 +1,15 @@
+import random
+
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'web/web-index.html')
+    dogs = [
+        'https://img.freepik.com/free-photo/cute-shepherd-dog-posing-isolated-white-background_155003-46179.jpg?size=626&ext=jpg',
+        'https://img.freepik.com/premium-photo/golden-retriever-dog-sitting-floor-isolated_8595-251.jpg?size=626&ext=jpg'
+
+    ]
+    context = {
+        'dog_image': random.choice(dogs),
+    }
+    return render(request, 'web/web-index.html',context)
