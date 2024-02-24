fc22c4eb0f6613abdd60591561a61d9051f89af9
kdkaanev <kdkaanev@github.com>
2024-02-24 11:16:24 +0200

    plan app first

--- exam/exam_app/exam_app/web/views.py
+++ exam/exam_app/exam_app/web/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from exam_app.user_profile.models import Profile
+
+
+
+
+
+
+
 # Create your views here.
+def base(request):
+    profile = Profile.objects.first()
+
+    context = {
+        'profile': profile
+    }
+    return render(request, 'base.html', context)
+
+
+def index(request):
+    return render(request, 'web/index.html')
